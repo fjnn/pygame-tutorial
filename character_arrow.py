e85491c979c030aa9fac32dea6db8a7e18f63224
gizem-thinkerpad <gizem.ates@hvl.no>
2025-01-23 00:46:58 +0100

    animation

--- character_arrow.py
+++ character_arrow.py
@@ -1,18 +1,21 @@
 import pygame
 
+
 class Character:
     def __init__(self, screen, x, y, size) -> None:
         self.screen = screen
         # self.color = (255, 0, 0)
-        self.image = pygame.image.load("assets/hotdog.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.images = []
+        self.images.append(pygame.image.load("assets/hotdog.png").convert_alpha())
+        self.images.append(pygame.image.load("assets/hotdog2.png").convert_alpha())
+        self.images = [pygame.transform.scale(img, (100, 100)) for img in self.images]
         self.x = x
         self.y = y
         self.size = size
 
     def update(self):
         # pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.size)
-        self.screen.blit(self.image, (self.x, self.y))
+        self.screen.blit(self.images[pygame.time.get_ticks()%2], (self.x, self.y))
 
         # grab keyboard input for left and right through a and d
         keys = pygame.key.get_pressed()
